3578045d1b25d5eab407c9556bea7b750e1ca492
Toeygenx <thanlast99@gmail.com>
2025-04-10 14:27:17 +0700

    most done

--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -1,6 +1,6 @@
 // Loading animation
 const shimmer =
-  'before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/60 before:to-transparent';
+  "before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/60 before:to-transparent";
 
 export function CardSkeleton() {
   return (
@@ -198,7 +198,7 @@ export function InvoicesTableSkeleton() {
                   scope="col"
                   className="relative pb-4 pl-3 pr-6 pt-2 sm:pr-6"
                 >
-                  <span className="sr-only">Edit</span>
+                  <span className="sr-only"></span>
                 </th>
               </tr>
             </thead>
